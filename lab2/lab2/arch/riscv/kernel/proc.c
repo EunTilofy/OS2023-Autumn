3645b07877a2b6f660a2007886e83fa70fc51a80
itolfy <papercloud1314@gmail.com>
2023-11-07 14:13:51 +0000

    rm _asm

--- lab2/lab2/arch/riscv/kernel/proc.c
+++ lab2/lab2/arch/riscv/kernel/proc.c
@@ -68,7 +68,7 @@ void dummy() {
     int last_counter = -1;
     while(1) {
         if ((last_counter == -1 || current->counter != last_counter) && current->counter > 0) {
-            printk("dummy : %d %d %d %d\n", task[1]->counter, task[2]->counter, task[3]->counter, task[4]->counter);
+            //printk("dummy : %d %d %d %d\n", task[1]->counter, task[2]->counter, task[3]->counter, task[4]->counter);
             if(current->counter == 1){
                 --(current->counter);   // forced the counter to be zero if this thread is going to be scheduled
             }                           // in case that the new counter is also 1, leading the information not printed.
@@ -79,51 +79,53 @@ void dummy() {
     }
 }
 
-void __switch_to(struct task_struct* prev, struct task_struct* next) {
-    __asm__ volatile (
-        "\tsd sp, %[prev_sp]\n"
-        "\tsd ra, %[prev_ra]\n"
-        "\tsd s0, %[prev_s0]\n"
-        "\tsd s1, %[prev_s1]\n"
-        "\tsd s2, %[prev_s2]\n"
-        "\tsd s3, %[prev_s3]\n"
-        "\tsd s4, %[prev_s4]\n"
-        "\tsd s5, %[prev_s5]\n"
-        "\tsd s6, %[prev_s6]\n"
-        "\tsd s7, %[prev_s7]\n"
-        "\tsd s8, %[prev_s8]\n"
-        "\tsd s9, %[prev_s9]\n"
-        "\tsd s10, %[prev_s10]\n"
-        "\tsd s11, %[prev_s11]\n"
-    :   [prev_sp] "=m" (prev->thread.sp),     [prev_ra] "=m" (prev->thread.ra),     [prev_s0] "=m" (prev->thread.s[0]),
-        [prev_s1] "=m" (prev->thread.s[1]),   [prev_s2] "=m" (prev->thread.s[2]),   [prev_s3] "=m" (prev->thread.s[3]),
-        [prev_s4] "=m" (prev->thread.s[4]),   [prev_s5] "=m" (prev->thread.s[5]),   [prev_s6] "=m" (prev->thread.s[6]),
-        [prev_s7] "=m" (prev->thread.s[7]),   [prev_s8] "=m" (prev->thread.s[8]),   [prev_s9] "=m" (prev->thread.s[9]),
-        [prev_s10] "=m" (prev->thread.s[10]),  [prev_s11] "=m" (prev->thread.s[11])
-    :
-    );
-    __asm__ volatile (
-        "\tld ra, %[next_ra]\n"
-        "\tld sp, %[next_sp]\n"
-        "\tld s0, %[next_s0]\n"
-        "\tld s1, %[next_s1]\n"
-        "\tld s2, %[next_s2]\n"
-        "\tld s3, %[next_s3]\n"
-        "\tld s4, %[next_s4]\n"
-        "\tld s5, %[next_s5]\n"
-        "\tld s6, %[next_s6]\n"
-        "\tld s7, %[next_s7]\n"
-        "\tld s8, %[next_s8]\n"
-        "\tld s9, %[next_s9]\n"
-        "\tld s10, %[next_s10]\n"
-        "\tld s11, %[next_s11]\n"
-    ::  [next_sp] "m" (next->thread.sp),     [next_ra] "m" (next->thread.ra),     [next_s0] "m" (next->thread.s[0]),
-        [next_s1] "m" (next->thread.s[1]),   [next_s2] "m" (next->thread.s[2]),   [next_s3] "m" (next->thread.s[3]),
-        [next_s4] "m" (next->thread.s[4]),   [next_s5] "m" (next->thread.s[5]),   [next_s6] "m" (next->thread.s[6]),
-        [next_s7] "m" (next->thread.s[7]),   [next_s8] "m" (next->thread.s[8]),   [next_s9] "m" (next->thread.s[9]),
-        [next_s10] "m" (next->thread.s[10]),  [next_s11] "m" (next->thread.s[11])
-    );
-}
+extern void __switch_to(struct task_struct* prev, struct task_struct* next);
+
+// void __switch_to(struct task_struct* prev, struct task_struct* next) {
+//     __asm__ volatile (
+//         "\tsd sp, %[prev_sp]\n"
+//         "\tsd ra, %[prev_ra]\n"
+//         "\tsd s0, %[prev_s0]\n"
+//         "\tsd s1, %[prev_s1]\n"
+//         "\tsd s2, %[prev_s2]\n"
+//         "\tsd s3, %[prev_s3]\n"
+//         "\tsd s4, %[prev_s4]\n"
+//         "\tsd s5, %[prev_s5]\n"
+//         "\tsd s6, %[prev_s6]\n"
+//         "\tsd s7, %[prev_s7]\n"
+//         "\tsd s8, %[prev_s8]\n"
+//         "\tsd s9, %[prev_s9]\n"
+//         "\tsd s10, %[prev_s10]\n"
+//         "\tsd s11, %[prev_s11]\n"
+//     :   [prev_sp] "=m" (prev->thread.sp),     [prev_ra] "=m" (prev->thread.ra),     [prev_s0] "=m" (prev->thread.s[0]),
+//         [prev_s1] "=m" (prev->thread.s[1]),   [prev_s2] "=m" (prev->thread.s[2]),   [prev_s3] "=m" (prev->thread.s[3]),
+//         [prev_s4] "=m" (prev->thread.s[4]),   [prev_s5] "=m" (prev->thread.s[5]),   [prev_s6] "=m" (prev->thread.s[6]),
+//         [prev_s7] "=m" (prev->thread.s[7]),   [prev_s8] "=m" (prev->thread.s[8]),   [prev_s9] "=m" (prev->thread.s[9]),
+//         [prev_s10] "=m" (prev->thread.s[10]),  [prev_s11] "=m" (prev->thread.s[11])
+//     :
+//     );
+//     __asm__ volatile (
+//         "\tld ra, %[next_ra]\n"
+//         "\tld sp, %[next_sp]\n"
+//         "\tld s0, %[next_s0]\n"
+//         "\tld s1, %[next_s1]\n"
+//         "\tld s2, %[next_s2]\n"
+//         "\tld s3, %[next_s3]\n"
+//         "\tld s4, %[next_s4]\n"
+//         "\tld s5, %[next_s5]\n"
+//         "\tld s6, %[next_s6]\n"
+//         "\tld s7, %[next_s7]\n"
+//         "\tld s8, %[next_s8]\n"
+//         "\tld s9, %[next_s9]\n"
+//         "\tld s10, %[next_s10]\n"
+//         "\tld s11, %[next_s11]\n"
+//     ::  [next_sp] "m" (next->thread.sp),     [next_ra] "m" (next->thread.ra),     [next_s0] "m" (next->thread.s[0]),
+//         [next_s1] "m" (next->thread.s[1]),   [next_s2] "m" (next->thread.s[2]),   [next_s3] "m" (next->thread.s[3]),
+//         [next_s4] "m" (next->thread.s[4]),   [next_s5] "m" (next->thread.s[5]),   [next_s6] "m" (next->thread.s[6]),
+//         [next_s7] "m" (next->thread.s[7]),   [next_s8] "m" (next->thread.s[8]),   [next_s9] "m" (next->thread.s[9]),
+//         [next_s10] "m" (next->thread.s[10]),  [next_s11] "m" (next->thread.s[11])
+//     );
+// }
 
 void switch_to(struct task_struct* next) {
     /* YOUR CODE HERE */
@@ -166,63 +168,53 @@ void do_timer(void) {
 #ifdef DSJF
 void schedule(void) {
     /* YOUR CODE HERE */
-    int i;
-    int next_thread_id;
+    int next_id = -1;
     uint64 min_time = (1ull << 50);
 
     printk("schedule / counter : %d %d %d %d\n", task[1]->counter, task[2]->counter, task[3]->counter, task[4]->counter);
 
-    for(i = 1; i < NR_TASKS; i++) {
-        if(task[i]->counter>0)
-            break;
+    for(int i = 1; i < NR_TASKS; ++i) {
+        if(task[i]->counter > 0 && task[i]->state == TASK_RUNNING &&
+            (next_id==-1 || task[i]->counter < task[next_id]->counter)) {
+            next_id = i;
+        }
     }
-    if(i == NR_TASKS) {
-        for(i = 1; i < NR_TASKS; i++)
-        {
+    if(~next_id) {
+        printk("switch to [PID = %d COUNTER = %d]\n", next_id, task[next_id]->counter);
+        switch_to(task[next_id]);
+    } else {
+        for(int i = 1; i < NR_TASKS; ++i) {
             task[i]->state = TASK_RUNNING;
             task[i]->counter = rand();
             printk("SET [PID = %d COUNTER = %d]\n", i, task[i]->counter);
         }
     }
-
-    for(i = 1; i < NR_TASKS; i++) {
-        if(task[i]->state == TASK_RUNNING && task[i]->counter < min_time) {
-            min_time = task[i]->counter;
-            next_thread_id = i;
-        }
-    }
-    printk("switch to [PID = %d COUNTER = %d]\n", next_thread_id, task[next_thread_id]->counter);
-    switch_to(task[next_thread_id]);
 }
 #endif
 
 #ifdef DPRIORITY
 void schedule(void) {
     /* YOUR CODE HERE */
-    int i;
-    int next_thread_id;
-    uint64 max_priority = 0;
+    int next_id = -1;
+    uint64 min_time = (1ull << 50);
 
-    for(i = 1; i < NR_TASKS; i++) {
-        if(task[i]->counter)
-            break;
+    printk("schedule / counter : %d %d %d %d\n", task[1]->counter, task[2]->counter, task[3]->counter, task[4]->counter);
+
+    for(int i = 1; i < NR_TASKS; ++i) {
+        if(task[i]->counter > 0 && task[i]->state == TASK_RUNNING &&
+            (next_id==-1 || task[i]->priority > task[next_id]->priority)) {
+            next_id = i;
+        }
     }
-    if(i == NR_TASKS) {
-        for(i = 1; i < NR_TASKS; i++)
-        {
+    if(~next_id) {
+        printk("switch to [PID = %d PRIORITY = %d COUNTER = %d]\n", next_id, max_priority, task[next_id]->counter);
+        switch_to(task[next_id]);
+    } else {
+        for(int i = 1; i < NR_TASKS; ++i) {
             task[i]->state = TASK_RUNNING;
             task[i]->counter = rand();
             printk("SET [PID = %d PRIORITY = %d COUNTER = %d]\n", i, task[i]->priority, task[i]->counter);
         }
     }
-
-    for(i = 1; i < NR_TASKS; i++) {
-        if(task[i]->state == TASK_RUNNING && task[i]->priority > max_priority) {
-            max_priority = task[i]->priority;
-            next_thread_id = i;
-        }
-    }
-    printk("switch to [PID = %d PRIORITY = %d COUNTER = %d]\n", next_thread_id, max_priority, task[next_thread_id]->counter);
-    switch_to(task[next_thread_id]);
 }
 #endif
